3d6e27ce77caac8fe730747480c787e6f75b46dc
unlucky_ma_dude <Achra@HRAF>
2024-04-04 12:59:59 +0000

    Add project files.

---
+++ Achraf Jabrawi/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Achraf_Jabrawi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Achraf Jabrawi/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Achraf_Jabrawi
+{
+     {
+        public MainWindow() => InitializeComponent();
+
+    void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SubmitButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(nameTextBox.Text) ||
+            string.IsNullOrWhiteSpace(ageTextBox.Text) ||
+            !int.TryParse(ageTextBox.Text, out int age) || age < 0)
+        {
+            MessageBox.Show(" Age must be a non-negative integer.");
+            return;
+        }
+
+        
+        string data = $"Name: {nameTextBox.Text}, "
+                      + $"Age: {age}, "
+                      + $"Height: {heightSlider.Value}, "
+                      + $"Weight: {weightSlider.Value}";
+        object dataListBox = null;
+        dataListBox.Items.Add(data);
+
+
+        object nameTextBox = null;
+        nameTextBox.Clear();
+        object ageTextBox = null;
+        ageTextBox.Clear();
+        object heightSlider = null;
+        heightSlider.Value = 100;
+        object weightSlider = null;
+        weightSlider.Value = 30;
+    }
+}
+}
